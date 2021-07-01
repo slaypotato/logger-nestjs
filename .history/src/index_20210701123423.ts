24892e910c85c97ee5b215c6ea422e9cbcb09e44
Rafael <rafael.morales@inmetrics.com.br>
2021-07-01 12:39:35 -0300

    fix: updated index.ts

---
+++ .history/src/index_20210701123423.ts
@@ -0,0 +1,5 @@
+// export public api from here
+// for example:
+// export * from './decorators';
+export * from './logger.module';
+export * from './logger.service';
--- .history/src/index_20210701123753.ts
+++ .history/src/index_20210701123753.ts
@@ -5,5 +5,3 @@ export * from './logger.module';
 export * from './logger.service';
 export * from './lib/LogFormat';
 export * from './lib/timestampAdjust';
-export * from './dtos/inputLogger.dto';
-export * from './dtos/optionsLogger.dto';
